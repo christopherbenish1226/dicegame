ab1df4007dc3a930cdc638b65af8cd5bf984b563
Chris Benish <punando2003@yahoo.com>
2020-09-16 06:24:33 -0500

    added onload function change page layout centered page remove background added messages

--- main.js
+++ main.js
@@ -13,7 +13,27 @@
      imgDice1.src = "images/dice" + d1 + ".png";
      imgDice2.src = "images/dice" + d2 + ".png";
 
+     var message = "";
+     var sum = d1 + d2;
 
+
+     if (d1 === 1 && d2 === 1) {
+         message = "Snake Eyes";
+
+
+     } else if (d1 === 2 && d2 === 2) {
+         message = "duce";
+
+     } else if (sum === 7) {
+         message = "Lucky 7";
+     } else {
+
+         message = sum;
+     }
+
+
+
+     document.getElementById("status").innerHTML = message;
  }
 
 
